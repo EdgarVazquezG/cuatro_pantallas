b5a937f7dbe9cfeb5987a69f7a2fc4a195010407
Edgar <edgarvazquez52614@gmail.com>
2026-05-29 12:21:21 -0600

    Update ProductosView.swift

--- practica de 4 pantallas/ProductosView.swift
+++ practica de 4 pantallas/ProductosView.swift
@@ -1,9 +1,8 @@
-//
-//  ProductosView.swift
-//  secuencia_de_pantallas
-//
-//  Created by Jose Daniel Espinoza Gomez on 13/02/26.
-//
+//Roberto Uriel Alcázar Hernández
+//Jhiovani Alexander Bautista Coutiño
+//Henry Rodrigo Gordillo Villatoro
+//Edgar Adrián Vázquez González
+
 
 import SwiftUI
 
