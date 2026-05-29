48aa49652bdf884ff08aee1a41436b7fa68ea6ff
Edgar <edgarvazquez52614@gmail.com>
2026-05-29 12:14:58 -0600

    Update ConfigView.swift

--- practica de 4 pantallas/ConfigView.swift
+++ practica de 4 pantallas/ConfigView.swift
@@ -1,9 +1,7 @@
-//
-//  ConfigView.swift
-//  secuencia_de_pantallas
-//
-//  Created by Jose Daniel Espinoza Gomez on 13/03/26.
-//
+//Roberto Uriel Alcázar Hernández
+//Jhiovani Alexander Bautista Coutiño
+//Henry Rodrigo Gordillo Villatoro
+//Edgar Adrián Vázquez González
 
 import SwiftUI
 
