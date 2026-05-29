492571989920eb126ad89ab57dcdb2cad30a3cac
Edgar <edgarvazquez52614@gmail.com>
2026-05-29 12:17:06 -0600

    Update PostFormView.swift

--- practica de 4 pantallas/PostFormView.swift
+++ practica de 4 pantallas/PostFormView.swift
@@ -1,9 +1,8 @@
-//
-//  PostFormView.swift
-//  secuencia_de_pantallas
-//
-//  Created by Jose Daniel Espinoza Gomez on 26/03/26.
-//
+//Roberto Uriel Alcázar Hernández
+//Jhiovani Alexander Bautista Coutiño
+//Henry Rodrigo Gordillo Villatoro
+//Edgar Adrián Vázquez González
+
 import SwiftUI
 
 struct PostFormView: View {
